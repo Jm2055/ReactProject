d680cb2a66e95ac7f12d2e30179d3dbdbe080dfb
Jamie Mcintosh <jm2055@hw.ac.uk>
2024-02-21 16:11:28 +0000

    Props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,35 @@
 import './App.css';
+import { PropTypes } from "prop-types";
 import Method_Info from './Components/Method_Info.js';
 import Class_Info from './Components/Class_Info.js';
 
 function App() {
   return (
     <div className="App">
-      <Method_Info />  
-      <Class_Info />  
+      <Class_Info title ="The goat"/>   
+      <AddInput text="Hi" number= {4} /> 
       <AddInput /> 
+      <AddInput text="Mumma" /> 
     </div>
   );
 }
 
-function AddInput() {
-  const value = "fusion is the goat";
+function AddInput(props) {
   return (
     <form>
       <label for ="text-form">Type something pls: </label>
-      <input type="text" value = {value} id="text-form" />
+      <input type="text" value = {props.text} id="text-form" />
+      <p>{props.number}</p>
     </form>
   );
 }
+AddInput.defaultProps = {
+  number: 11,
+  text: "default"
+}
 
-
+AddInput.propTypes = {
+  number: PropTypes.number, 
+  text: PropTypes.string,
+}
 export default App;
--- my-app/src/Components/Class_Info.js
+++ my-app/src/Components/Class_Info.js
@@ -1,13 +1,18 @@
 import React from "react";
 class Class_Info extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
   render() {
-    const title = "He is the goat";
+    const title = this.props.title;
     const showTitle = true;
+
     if (showTitle) {
       return (
         <div>
           <h1>Fusion Alli</h1>
-          <h2>{showTitle ? title : "No Title"}</h2>
+          <h2>{title}</h2>
           <h2>{20 + 6}</h2>
           <p>22/23 POTY Winner</p>
         </div>
@@ -16,6 +21,10 @@ class Class_Info extends React.Component {
       return <p>empty</p>;
     }
   }
+}
+
+Class_Info.defaultProps = {
+    title: "default"
 }
 
 export default Class_Info;
